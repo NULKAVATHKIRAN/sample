ec693c29992ddfcce989ab029f30e6f451e312f6
NULKAVATHKIRAN <kirankumarnaik143kumarnaik@gmail.com>
2025-07-15 11:16:55 +0530

    added generators and decorators

--- sample.py
+++ sample.py
@@ -1,3 +1,25 @@
-print("hello world")
-def hello():
-    return "hello"
+def decor(func):
+    def inner():
+        value = func()
+        return value+2
+    return inner
+
+@decor
+def value():
+    return 10
+
+
+def generator():
+    for i in range(1,11):
+        yield i
+
+def sample():
+    yield "A"
+    yield "B"
+    yield "C"
+
+temp=sample() 
+print(next(temp))
+print(next(temp))
+print(next(temp))
+
